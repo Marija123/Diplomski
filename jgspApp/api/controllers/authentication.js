b600b4ab2e14fcde541df0fc4abbc236b69fbb85
Marija123 <marijadjurkovic503@gmail.com>
2019-08-15 16:14:37 +0200

    Inital commit, register,login,logout

---
+++ jgspApp/api/controllers/authentication.js
@@ -0,0 +1,69 @@
+var passport = require('passport');
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+
+var sendJSONresponse = function(res, status, content)
+{
+    res.status(status);
+    res.json(content);
+}
+
+module.exports.register = function(req, res)
+{
+    if(!req.body.name || !req.body.email || !req.body.password || !req.body.surname || !req.body.address  || !req.body.birthday || !req.body.role) {
+        sendJSONresponse(res, 400, {
+            "message": "All fields required"
+        });
+        return;
+    }
+
+    var user = new User();
+
+    user.name = req.body.name;
+    user.email = req.body.email;
+    user.surname = req.body.surname;
+    user.address = req.body.address;
+    user.birthday = req.body.birthday;
+    //user.image = req.body.image;
+    //user.activated = req.body.activated;
+    user.role = req.body.role;
+
+    user.setPassword(req.body.password);
+
+    user.save(function(err){
+        var token;
+        token = user.generateJwt();
+        res.status(200);
+        res.json({
+            "token" : token
+        });
+    });
+};
+
+module.exports.login = function(req, res){
+    if(!req.body.email || !req.body.password) {
+        sendJSONresponse(res, 400, {
+            "message": "All fields required"
+        });
+        return;
+    }
+
+    passport.authenticate('local', function(err, user, info){
+        var token;
+        if(err)
+        {
+            res.status(404).json(err);
+            return;
+        }
+
+        if(user){
+            token = user.generateJwt();
+            res.status(200);
+            res.json({
+                "token" : token
+            });
+        } else {
+            res.status(401).json(info);
+        }
+    })(req,res);
+};
---
+++ jgspApp/api/model/user.js
@@ -0,0 +1,74 @@
+var mongoose = require('mongoose');
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
+
+var userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    surname: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    activated: {
+        type: String,
+        required: false
+    },
+    role: {
+        type: String,
+        required: true
+    },
+    birthday: {
+        type: Date,
+        required: true
+    },
+    image: {
+        type: String,
+        required: false
+    },
+    hash:String,
+    salt: String
+});
+
+//lista karata, passenger type
+
+userSchema.methods.setPassword = function(password){
+    this.salt = crypto.randomBytes(16).toString('hex'); //?
+    this.hash = crypto.pbkdf2Sync(password,this.salt,1000,64,'sha512').toString('hex');
+
+}
+
+userSchema.methods.validPassword = function(password){
+    var hash = crypto.pbkdf2Sync(password,this.salt,1000, 64, 'sha512').toString('hex');
+    return this.hash === hash;
+}
+
+userSchema.methods.generateJwt = function() {
+    var expiry = new Date();
+    expiry.setDate(expiry.getDate() + 7);
+
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        name: this.name,
+        surname: this.surname,
+        address: this.address,
+        birthday: this.birthday,
+        //image: this.image,
+        //activated: this.activated,
+        role: this.role,
+        exp: parseInt(expiry.getTime() / 1000),
+    }, "MY_SECRET");
+};
+
+mongoose.model('User', userSchema);
